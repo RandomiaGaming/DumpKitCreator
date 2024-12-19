cb1ebee9500c9d0162d796c03e385c8173665730
RandomiaGaming <randomiagaming@gmail.com>
2024-12-18 16:15:32 -0800

    Init commit

---
+++ GithubHelper.cs
@@ -0,0 +1,159 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DumpKitCreator
+{
+    public static class GithubHelper
+    {
+        private static readonly WebClient reusableClient = CreateClient();
+        private static Stopwatch downloadTimer = Stopwatch.StartNew();
+        private static Random RNG = new Random();
+        private static long nextValidDownloadTime = 0;
+        private static WebClient CreateClient()
+        {
+            WebClient output = new WebClient();
+
+            //Set a more reasonable user agent.
+            output.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.1234.56 Safari/537.36");
+
+            //Accept some file types
+            //output.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+
+            //Use en-us
+            //output.Headers.Add("Accept-Language", "en-US,en;q=0.8");
+
+            //Set our referrer to a reddit post
+            output.Headers.Add("Referer", "https://www.google.com/search?q=Lockpick+rcm&sca_esv=575056345&sxsrf=AM9HkKmNIcXz6B9LvzotIQ1biQ1mhOE0YA%3A1697767548697&source=hp&ei=fOAxZefiH5yr0PEPr4eesAs&iflsig=AO6bgOgAAAAAZTHujGrWSSvNkNTdb52oIZiapAobzKIB&ved=0ahUKEwjnkJD9xIOCAxWcFTQIHa-DB7YQ4dUDCAw&uact=5&oq=Lockpick+rcm&gs_lp=Egdnd3Mtd2l6IgxMb2NrcGljayByY20yBRAAGIAEMgoQABiABBgUGIcCMgUQABiABDIKEAAYgAQYFBiHAjIFEAAYgAQyBRAAGIAEMgoQABiABBixAxgKMgUQABiABDIFEAAYgAQyBRAAGIAESK0bUP0FWL0ZcAF4AJABAJgBS6AB-wWqAQIxMrgBA8gBAPgBAagCCsICBxAjGOoCGCfCAgcQIxiKBRgnwgIEECMYJ8ICBxAAGIoFGEPCAg4QLhiABBixAxjHARjRA8ICERAuGIAEGLEDGIMBGMcBGNEDwgINEAAYgAQYFBiHAhixA8ICDhAAGIAEGLEDGIMBGMkDwgIIEAAYigUYkgPCAggQABiABBixA8ICCxAAGIAEGLEDGMkDwgIIEAAYgAQYkgPCAgsQABiABBixAxiDAcICDRAAGIoFGLEDGIMBGEPCAgoQABiKBRixAxhDwgILEC4YgAQYsQMYgwHCAgsQLhiDARixAxiABMICCxAAGIoFGLEDGIMBwgIIEAAYigUYsQPCAgkQABiKBRgKGEPCAgUQLhiABA&sclient=gws-wiz");
+
+            return output;
+        }
+        private static void PreDownload()
+        {
+            //Wait until its okay to download something by sleeping for 100 miliseconds.
+            while (downloadTimer.ElapsedTicks < nextValidDownloadTime)
+            {
+                Thread.Sleep(100);
+            }
+        }
+        private static void PostDownload()
+        {
+            //Random delay between 0.5 seconds and 3 seconds.
+            nextValidDownloadTime = downloadTimer.ElapsedTicks + RNG.Next(5000000, 30000000);
+        }
+        public static ReleaseMeta GetLatestReleaseMeta(string author, string repo)
+        {
+            string endpointURL = $"https://api.github.com/repos/{author}/{repo}/releases/latest";
+            PreDownload();
+            string json = reusableClient.DownloadString(endpointURL);
+            PostDownload();
+            return JsonConvert.DeserializeObject<ReleaseMeta>(json);
+        }
+        public static void DownloadFileFromRelease(ReleaseMeta releaseMeta, string targetFileName, string destinationFilePath)
+        {
+            string sourceFileURL = null;
+            foreach (ReleaseMeta.ReleaseMetaAsset asset in releaseMeta.assets)
+            {
+                if (asset.name == targetFileName)
+                {
+                    sourceFileURL = asset.browser_download_url;
+                    break;
+                }
+            }
+            if (sourceFileURL == null)
+            {
+                throw new Exception($"Github release did not contain a file named \"{targetFileName}\"");
+            }
+            PreDownload();
+            reusableClient.DownloadFile(sourceFileURL, destinationFilePath);
+            PostDownload();
+        }
+
+
+        public class ReleaseMeta
+        {
+            public string url;
+            public string assets_url;
+            public string upload_url;
+            public string html_url;
+            public int id;
+            public ReleaseMetaAuthor author;
+            public string node_id;
+            public string tag_name;
+            public string target_commitish;
+            public string name;
+            public bool draft;
+            public bool prerelease;
+            public DateTime created_at;
+            public DateTime published_at;
+            public List<ReleaseMetaAsset> assets;
+            public string tarball_url;
+            public string zipball_url;
+            public string body;
+            public class ReleaseMetaAsset
+            {
+                public string url;
+                public int id;
+                public string node_id;
+                public string name;
+                public object label;
+                public ReleaseMetaUploader uploader;
+                public string content_type;
+                public string state;
+                public int size;
+                public int download_count;
+                public DateTime created_at;
+                public DateTime updated_at;
+                public string browser_download_url;
+                public class ReleaseMetaUploader
+                {
+                    public string login;
+                    public int id;
+                    public string node_id;
+                    public string avatar_url;
+                    public string gravatar_id;
+                    public string url;
+                    public string html_url;
+                    public string followers_url;
+                    public string following_url;
+                    public string gists_url;
+                    public string starred_url;
+                    public string subscriptions_url;
+                    public string organizations_url;
+                    public string repos_url;
+                    public string events_url;
+                    public string received_events_url;
+                    public string type;
+                    public bool site_admin;
+                }
+            }
+            public class ReleaseMetaAuthor
+            {
+                public string login;
+                public int id;
+                public string node_id;
+                public string avatar_url;
+                public string gravatar_id;
+                public string url;
+                public string html_url;
+                public string followers_url;
+                public string following_url;
+                public string gists_url;
+                public string starred_url;
+                public string subscriptions_url;
+                public string organizations_url;
+                public string repos_url;
+                public string events_url;
+                public string received_events_url;
+                public string type;
+                public bool site_admin;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,303 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RomHelper
+{
+    public static class Program
+    {
+        public const string ProgramFullName = "Dump Kit Creator - Version 1.0.0";
+        public static bool InShell = true;
+        public static string SwitchSDPath = GetDefaultSwitchSDPath();
+        private static string GetDefaultSwitchSDPath()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady)
+                {
+                    string nintendoFolder = Path.Combine(drive.RootDirectory.FullName, "Nintendo\\Contents\\registered");
+                    if (Directory.Exists(nintendoFolder))
+                    {
+                        return drive.RootDirectory.FullName;
+                    }
+                }
+            }
+            return null;
+        }
+        public static void ConvertAllAudioFiles(string sourceFolderPath, string destinationFolderPath, string audioConverterPath, string commandTemplate)
+        {
+            List<string> files = new List<string>(Directory.GetFiles(sourceFolderPath, "*", SearchOption.AllDirectories));
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                if (!files[i].ToLower().EndsWith(".bwav"))
+                {
+                    files.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            List<Process> children = new List<Process>();
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                string source = files[i];
+
+                string destination = files[i].Substring(sourceFolderPath.Length);
+                destination = destinationFolderPath + destination;
+                destination = destination.Replace(".bwav", ".wav");
+
+                string folder = Path.GetDirectoryName(destination);
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                if (!File.Exists(destination))
+                {
+                    while (children.Count > 25)
+                    {
+                        for (int j = 0; j < children.Count; j++)
+                        {
+                            if (children[j].HasExited)
+                            {
+                                children.RemoveAt(j);
+                                j--;
+                            }
+                        }
+                    }
+
+                    string command = commandTemplate.Replace("%source%", source).Replace("%destination%", destination);
+
+                    ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                    processStartInfo.FileName = audioConverterPath;
+                    processStartInfo.Arguments = command;
+                    processStartInfo.UseShellExecute = true;
+                    processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    processStartInfo.CreateNoWindow = true;
+
+                    children.Add(Process.Start(processStartInfo));
+                }
+
+                Console.WriteLine($"{i} of {files.Count}");
+            }
+        }
+        public sealed class ExtensionInfo
+        {
+            public string Extension => _extension;
+            private string _extension = null;
+            public int Count => _count;
+            private int _count = 0;
+            public ExtensionInfo(string extension)
+            {
+                _extension = extension;
+                _count = 0;
+            }
+            public void Increment()
+            {
+                _count++;
+            }
+        }
+        public static void FilesExts(string sourceFolderPath)
+        {
+            List<string> files = new List<string>(Directory.GetFiles(sourceFolderPath, "*", SearchOption.AllDirectories));
+            List<ExtensionInfo> extensionInfo = new List<ExtensionInfo>();
+
+            for (int i = 0; i < files.Count; i++)
+            {
+                string file = files[i];
+                string ext = Path.GetExtension(file);
+
+                bool foundExtEntry = false;
+
+                for (int j = 0; j < extensionInfo.Count; j++)
+                {
+                    if (extensionInfo[j].Extension == ext)
+                    {
+                        extensionInfo[j].Increment();
+                        foundExtEntry = true;
+                        break;
+                    }
+                }
+
+                if (!foundExtEntry)
+                {
+                    ExtensionInfo newExtensionInfo = new ExtensionInfo(ext);
+                    newExtensionInfo.Increment();
+                    extensionInfo.Add(newExtensionInfo);
+                }
+            }
+
+            extensionInfo.Sort((ExtensionInfo a, ExtensionInfo b) => { return a.Count - b.Count; });
+
+            for (int i = 0; i < extensionInfo.Count; i++)
+            {
+                Console.WriteLine($"{extensionInfo[i].Count} files with extension {extensionInfo[i].Extension}.");
+            }
+            Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            FilesExts("C:\\Users\\RandomiaGaming\\AppData\\Roaming\\yuzu\\dump\\010015100B514000\\romfs");
+            Console.ReadLine();
+            return;
+            try
+            {
+                Console.WriteLine(ProgramFullName);
+                Console.WriteLine();
+
+                if (!(args is null) && args.Length > 0)
+                {
+                    string command = "";
+                    for (int i = 0; i < args.Length; i++)
+                    {
+                        command += args[i];
+                        if (i != args.Length - 1)
+                        {
+                            command += " ";
+                        }
+                    }
+                    InShell = false;
+                    RunCommand(command);
+                }
+
+                while (InShell)
+                {
+                    Console.Write(">");
+                    RunCommand(Console.ReadLine());
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteError($"Fatal error: {ex.Message}.");
+            }
+        }
+        public static void RunCommand(string command)
+        {
+            try
+            {
+                string commandName = command.ToLower();
+                string arguments = "";
+                for (int i = 0; i < command.Length; i++)
+                {
+                    if (command[i] == ' ')
+                    {
+                        commandName = command.Substring(0, i).ToLower();
+                        if (i != command.Length - 1)
+                        {
+                            arguments = command.Substring(i + 1);
+                        }
+                        break;
+                    }
+                }
+
+                if (commandName is "exit")
+                {
+                    ExitCommand(arguments);
+                }
+                else if (commandName is "help")
+                {
+                    HelpCommand(arguments);
+                }
+                else if (commandName is "selectsd")
+                {
+                    SelectSDCommand(arguments);
+                }
+                else if (commandName is "getalbum")
+                {
+                    GetAlbumCommand(arguments);
+                }
+                else if (commandName is "wheresd")
+                {
+                    WhereSDCommand(arguments);
+                }
+                else
+                {
+                    throw new Exception($"Invalid command \"{commandName}\"");
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteError($"Error: {ex.Message}.");
+            }
+        }
+        public static void WriteError(string error)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
+            Console.ForegroundColor = originalColor;
+        }
+        public static void WriteWarning(string warning)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(warning);
+            Console.ForegroundColor = originalColor;
+        }
+
+        public static void ExitCommand(string arguments)
+        {
+            if (arguments != "")
+            {
+                throw new Exception("Invalid arguments.");
+            }
+            InShell = false;
+        }
+        public static void HelpCommand(string arguments)
+        {
+            Console.WriteLine("No help for you. Get fukd.");
+        }
+        public static void SelectSDCommand(string arguments)
+        {
+            SwitchSDPath = arguments;
+        }
+        public static void GetAlbumCommand(string arguments)
+        {
+            string albumSourcePath = Path.Combine(SwitchSDPath, "Nintendo\\Album");
+            string albumDestinationPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "Nintedo Switch Album");
+
+            if (!Directory.Exists(albumDestinationPath))
+            {
+                Directory.CreateDirectory(albumDestinationPath);
+            }
+
+            foreach (string file in Directory.GetFiles(albumSourcePath, "*", SearchOption.AllDirectories))
+            {
+                File.Copy(file, Path.Combine(albumDestinationPath, Path.GetFileName(file)));
+            }
+        }
+        public static void WhereSDCommand(string arguments)
+        {
+            Console.WriteLine(SwitchSDPath);
+        }
+        public static void InstallDumpkitCommand(string arguments)
+        {
+
+        }
+        public static void UpdateAll(string arguments)
+        {
+
+        }
+        public static void InstallAll(string arguments)
+        {
+
+        }
+        public static void AtmosphereCommand(string arguments)
+        {
+
+        }
+        public static void HetakeCommand(string arguments)
+        {
+
+        }
+    }
+}
